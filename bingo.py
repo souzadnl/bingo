76513f23bfbc04ae2b53fa676506735e6eb1d534
Igor Bedon de Sousa <12danielsouzaalves@gmail.com>
2024-06-20 09:19:57 -0300

    Adicionando projeto

---
+++ bingo.py
@@ -0,0 +1,71 @@
+import random
+import sys
+
+def criar():
+    numeros = []
+    for numero in range(1, 76):
+        numeros.append(numero)
+    return numeros
+    
+    
+def sortear(numeros, numeros_sorteados):
+    if(numeros==[]):
+        print("Acabaram os números!")
+        sys.exit()
+    else:
+        numero_sorteado = random.choice(numeros)
+        print(numero_sorteado)
+        numeros_sorteados.append(numero_sorteado)
+        numeros.remove(numero_sorteado)
+    return numeros, numeros_sorteados
+
+
+def verificar_se_bateu(lista_sorteados):
+    while(True):
+        resposta = input("\nHouve um vencedor? Digite S para sim ou N para não.\n")
+        try:
+            if(resposta == "S" or resposta == "s"):
+                print("Muito bem. Aqui está a lista de números sorteados para garantir que ninguém comeu bola!")
+                for numero in lista_sorteados:
+                    print(numero)
+                return verificar_vencedor()
+                  
+                
+            elif(resposta == "N" or resposta == "n"):
+                print("Muito bem, vamos continuar\n")
+                return True
+            else:
+                print("Inserção inválida!\n")
+        except:
+            print("Inserção inválida!\n")
+            
+            
+def verificar_vencedor():          
+    while(True):
+        houve_vencedor = input("\nA pessoa ganhou mesmo? Digite S para sim ou N para não.\n")  
+        try:
+            if(houve_vencedor == "S" or houve_vencedor == "s"):
+                print("PARABÉNSS. Finalizando o jogo\n")
+                return False
+                
+            elif(houve_vencedor == "N" or houve_vencedor == "n"):
+                print("Certo, então vamos continuar\n")
+                return True
+                
+            else:
+                print("Inserção inválida!\n")
+        
+        except:
+            print("Inserção inválida!\n")
+    
+print("Bem-Vindo ao Bingão!")
+continuar = True
+numeros = criar()
+numeros_sorteados = []
+input("Aperte ENTER para sortear um número!")
+
+while continuar:
+    numeros, numeros_sorteados = sortear(numeros, numeros_sorteados)
+    continuar = verificar_se_bateu(numeros_sorteados)
+
+
